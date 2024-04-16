52d061bbdd6ff99383e922a88e4ce66b7f96d044
kontai <cho.kontai@gmail.com>
2024-04-17 01:29:38 +0800

    20240417 from pc

---
+++ Demo_d16/DelegetExample/DelegetExample/Progream3.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 以接口取代委託,降低耦合
+ */
+namespace DelegetExample
+{
+
+    internal class Progream2
+    {
+        public static void Main(string[] args)
+        {
+            IProductFactory pizzaFactory = new PizzaFactory();
+            IProductFactory toycarFactory = new ToyCarFactory();
+            WrapFactory factory = new WrapFactory();
+
+            Box box1 = factory.WrapProduct(pizzaFactory);
+            Box box2 = factory.WrapProduct(toycarFactory);
+
+
+            Console.WriteLine(box1.Product.Name);
+            Console.WriteLine(box2.Product.Name);
+        }
+    }
+
+    interface IProductFactory
+    {
+        Product Make();
+    }
+
+    class PizzaFactory : IProductFactory
+    {
+        public Product Make()
+        {
+            Product product = new Product();
+            product.Name = "pizza";
+            return product;
+        }
+    }
+
+    class ToyCarFactory : IProductFactory
+    {
+        public Product Make()
+        {
+            Product product = new Product();
+            product.Name = "toy car";
+            return product;
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+    }
+
+    class Box
+    {
+        public Product Product { get; set; }
+    }
+
+    class WrapFactory
+    {
+        /// <summary>
+        /// Wraps the product.
+        /// </summary>
+        /// <param name="getProduct">將返回值為Product的方法當作參數</param>
+        /// <returns></returns>
+        public Box WrapProduct(IProductFactory factory)
+        {
+            Box box = new Box();
+            box.Product = factory.Make();
+            return box;
+        }
+    }
+
+}
---
+++ Demo_d16/MethodParam2/MethodParam2/OutDemo2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodParam2
+{
+    internal class OutDemo2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pleae input first number=>");
+            string arg1 = Console.ReadLine();
+            double x = 0;
+            bool b1 = DoubleParser.MyTryParse(arg1, out x);
+            if (!b1)
+            {
+                Console.WriteLine("Number error!");
+                return;
+            }
+
+            Console.WriteLine("Pleae input second number=>");
+            string arg2 = Console.ReadLine();
+            double y = 0;
+            bool b2 = DoubleParser.MyTryParse(arg2, out y);
+            if (!b2)
+            {
+                Console.WriteLine("Number error!");
+                return;
+            }
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+        }
+    }
+
+    class DoubleParser
+    {
+        public static bool MyTryParse(string? str, out double result)
+        {
+            try
+            {
+                result = double.Parse(str);
+            }
+            catch
+            {
+                Console.WriteLine();
+                result = 0;
+                return false;
+            }
+            return true;
+        }
+    }
+}
