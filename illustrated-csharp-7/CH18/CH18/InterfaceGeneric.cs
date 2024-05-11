496f097238a33925a91864f2d2a41aef63e10655
kontai <cho.kontai@gmail.com>
2024-05-11 20:12:05 +0800

    update

--- illustrated-csharp-7/CH18/CH18/InterfaceGeneric.cs
+++ illustrated-csharp-7/CH18/CH18/InterfaceGeneric.cs
@@ -7,15 +7,37 @@ interface IMuIfc<T>
     T ReturnIt(T intValue);
 }
 
+
+// 泛型接口的實現必須唯一(S如果是int,則會發生衝突)
+/*      
+class Simple<S> : IMuIfc<int>, IMuIfc<S>
+{
+    public int ReturnIt(int intValue)
+    {
+        return intValue;
+    }
+
+    public S ReturnIt(S intValue)
+    {
+        return intValue;
+    }
+}
+*/
+
 class Simple : IMuIfc<int>, IMuIfc<string>  //非泛型接口
 {
     public int ReturnIt(int intValue)  //實現int泛型接口
     {
         return intValue;
     }
-    public string ReturnIt(string intValue)    //實現string泛型接口
+    public string ReturnIt(string stringValue)    //實現string泛型接口
     {
-        return intValue;
+        return stringValue;
+    }
+
+    public S ReturnIt(S sValue)
+    {
+        return sValue;
     }
 }
 public class InterfaceGeneric
--- illustrated-csharp-7/CH18/CH18/obj/Debug/net8.0/CH18.AssemblyInfo.cs
+++ illustrated-csharp-7/CH18/CH18/obj/Debug/net8.0/CH18.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CH18")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c088160b7f30cfb5dc1b8a4e99622f5401d76cd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755687d4e8991997023f41cde8835df73fbd8afb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CH18")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CH18")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
