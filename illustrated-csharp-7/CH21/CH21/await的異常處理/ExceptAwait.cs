73254ca4825230300ad666f7a29a7ae63ad0c796
kontai <cho.kontai@gmail.com>
2024-05-25 16:32:21 +0800

    update

---
+++ illustrated-csharp-7/CH21/CH21/await的異常處理/ExceptAwait.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH21.await的異常處理
+{
+    internal class ExceptAwait
+    {
+        public static void Main(string[] args)
+        {
+            Task t = Badasync();
+            t.Wait();
+            Console.WriteLine($"Task Status : {t.Status}");
+            Console.WriteLine($"Task IsFaulted: {t.IsFaulted}");
+        }
+        static async Task Badasync()
+        {
+            try
+            {
+                await Task.Run(() => { throw new Exception(); });
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Exception in BadAsync");
+            }
+        }
+    }
+}
--- illustrated-csharp-7/CH21/CH21/obj/Debug/net8.0/CH21.AssemblyInfo.cs
+++ illustrated-csharp-7/CH21/CH21/obj/Debug/net8.0/CH21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CH21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb128eb309e830f71a2ddc6ecffa9c01bec73f9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829e5bd473f163da633a59b71304d424d3c1f8ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CH21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CH21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
