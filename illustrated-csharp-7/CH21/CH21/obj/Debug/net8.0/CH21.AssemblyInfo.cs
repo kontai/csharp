11e7a11da0266ccece7756e6caec945d18f8fe4a
kontai <cho.kontai@gmail.com>
2024-05-26 20:09:39 +0800

    update

--- illustrated-csharp-7/CH21/CH21/obj/Debug/net8.0/CH21.AssemblyInfo.cs
+++ illustrated-csharp-7/CH21/CH21/obj/Debug/net8.0/CH21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CH21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829e5bd473f163da633a59b71304d424d3c1f8ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73254ca4825230300ad666f7a29a7ae63ad0c796")]
 [assembly: System.Reflection.AssemblyProductAttribute("CH21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CH21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
