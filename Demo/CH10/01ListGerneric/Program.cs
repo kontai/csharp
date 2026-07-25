2e67c88b663720bcdbbe479444e0d3af116c67bb
kontai <cho.kontai@gmail.com>
2026-07-25 09:10:08 +0800

    20260725

---
+++ Demo/CH10/01ListGerneric/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FunWithGenericCollections;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        UseGenericList();
+    }
+
+    private static void UseGenericList()
+    {
+        // Make a List of Person objects, filled with
+        // collection/object init syntax.
+        List<Person> people = new List<Person>()
+  {
+    new Person {FirstName= "Homer", LastName="Simpson", Age=47},
+    new Person {FirstName= "Marge", LastName="Simpson", Age=45},
+    new Person {FirstName= "Lisa", LastName="Simpson", Age=9},
+    new Person {FirstName= "Bart", LastName="Simpson", Age=8}
+  };
+        // Print out # of items in List.
+        Console.WriteLine("Items in list: {0}", people.Count);
+        // Enumerate over list.
+        foreach (Person p in people)
+        {
+            Console.WriteLine(p);
+        }
+        // Insert a new person.
+        Console.WriteLine("\n->Inserting new person.");
+        people.Insert(2, new Person { FirstName = "Maggie", LastName = "Simpson", Age = 2 });
+        Console.WriteLine("Items in list: {0}", people.Count);
+        // Copy data into a new array.
+        Person[] arrayOfPeople = people.ToArray();
+        foreach (Person p in arrayOfPeople)
+        {
+            Console.WriteLine("First Names: {0}", p.FirstName);
+        }
+    }
+}
--- Demo/CH9/LazyC lass/obj/Debug/net10.0/LazyC lass.AssemblyInfo.cs
+++ Demo/CH9/LazyC lass/obj/Debug/net10.0/LazyC lass.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LazyC lass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f31a52edeff880b11ba60af7d35af75276e6ed9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41fa23e26f6e99a178540f60feb6535ddb7146f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LazyC lass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LazyC lass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
