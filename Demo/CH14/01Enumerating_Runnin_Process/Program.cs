01f9d436108b77464763b9b16c193535bc55bf38
kontai <cho.kontai@gmail.com>
2026-08-10 11:00:45 +0800

    20260810

---
+++ Demo/CH14/01Enumerating_Runnin_Process/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+GetProcess();
+//GetProcessById(7499);
+
+static void GetProcess()
+{
+    var process = from pros in Process.GetProcesses() orderby pros.Id select pros;
+    foreach (var p in process)
+    {
+        Console.WriteLine($"-> PID: {p.Id}\tName: {p.ProcessName}");
+    }
+    if (process.TryGetNonEnumeratedCount(out int count))
+    {
+        Console.WriteLine("-> Total Process: " + count);
+    }
+}
+
+static void GetProcessById(int id)
+{
+    Process process = null;
+
+    try
+    {
+        process = Process.GetProcessById(id);
+        Console.WriteLine("Process Name: {0}", process?.ProcessName);
+    }
+    catch (ArgumentException e)
+    {
+        Console.WriteLine("找不到PID為 ({0}) 的程序，錯誤信息為: {1}", id, e.Message);
+    }
+}
