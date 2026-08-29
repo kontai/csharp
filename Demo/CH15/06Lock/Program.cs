359c1cb1dffe312e7cb97c5a4b0e497c17be5aa9
kontai <cho.kontai@gmail.com>
2026-08-29 09:02:38 +0800

    20260829

---
+++ Demo/CH15/06Lock/Program.cs
@@ -0,0 +1,16 @@
+﻿using MultsiThreadedPrinting;
+
+Printer p = new Printer();
+
+Thread[] threads = new Thread[5];
+
+for (int i = 0; i < 5; i++)
+{
+    threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
+    threads[i].Name = $"Worker thread #{i}";
+}
+
+foreach (var worker in threads)
+{
+    worker.Start();
+}
